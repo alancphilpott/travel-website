e3ff9198120f6824781699f491446a491bbf1206
alancphilpott <alanp1995@gmail.com>
2021-03-28 12:34:52 +0100

    toggle navbar/burger

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let controller, slideScene, pageScene
-let mouse = document.querySelector('.cursor')
+const mouse = document.querySelector('.cursor')
+const burger = document.querySelector('.burger')
 
 const animateSlides = () => {
   controller = new ScrollMagic.Controller()
@@ -73,7 +74,26 @@ const activeCursor = (e) => {
   }
 }
 
+const navToggle = (e) => {
+  if (!e.target.classList.contains('active')) {
+    e.target.classList.add('active')
+    gsap.to('.line1', 0.5, { rotate: '45', y: 5, background: 'black' })
+    gsap.to('.line2', 0.5, { rotate: '-45', y: -5, background: 'black' })
+    gsap.to('#logo', 1, { color: 'black' })
+    gsap.to('.nav-bar', 1, { clipPath: 'circle(2500px at 100% -10%)' })
+    document.body.classList.add('hide')
+  } else {
+    e.target.classList.remove('active')
+    gsap.to('.line1', 0.5, { rotate: '0', y: 0, background: 'white' })
+    gsap.to('.line2', 0.5, { rotate: '0', y: 0, background: 'white' })
+    gsap.to('#logo', 1, { color: 'white' })
+    gsap.to('.nav-bar', 1, { clipPath: 'circle(50px at 100% -10%)' })
+    document.body.classList.remove('hide')
+  }
+}
+
 window.addEventListener('mousemove', cursor)
 window.addEventListener('mouseover', activeCursor)
+burger.addEventListener('click', navToggle)
 
 animateSlides()
